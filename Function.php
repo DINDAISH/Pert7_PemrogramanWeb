f3476e2a4425940e5712cc2538e0c8d0160d9f8e
Dinda Nur Ishma <dindanurisma727@gmail.com>
2024-03-27 16:31:55 +0700

    first commit

---
+++ Function.php
@@ -0,0 +1,10 @@
+<?php
+// Mendefinisikan fungsi
+function greet($name) {
+    echo "Halo, $name!";
+}
+
+// Memanggil fungsi
+greet("John"); // Output: Halo, John!
+greet("Jane"); // Output: Halo, Jane!
+?>
